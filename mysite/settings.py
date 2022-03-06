821ab34878de2553143f095676160dfc71a561f6
Sherif Abdullah <85506496+sherif-abdallah@users.noreply.github.com>
2022-03-06 18:19:19 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,6 +77,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }}
+# Postgres
+import dj_database_url
+db_form_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_form_env)
+
 
 
 # Password validation
